f5b2d9822499ce6b66ccbaf08e7052c8747e62c2
Yoji Shidara <dara@shidara.net>
2015-02-02 20:50:51 +0900

    Go fmt

--- main.go
+++ main.go
@@ -1,83 +1,82 @@
 package main
 
 import (
-  "encoding/json"
-  "io/ioutil"
-  "log"
-  "net/http"
-  "os"
-  "path/filepath"
-  "text/template"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"text/template"
 )
 
 type Bindings struct {
-  TemplatesJson string
-  IndexJs       string
+	TemplatesJson string
+	IndexJs       string
 }
 
 func main() {
-  f, err   := os.Open("gene-attributes/index.html.erb")
-  if err != nil {
-    log.Fatal(err)
-  }
+	f, err := os.Open("gene-attributes/index.html.erb")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  t, err := ioutil.ReadAll(f)
-  if err != nil {
-    log.Fatal(err)
-  }
+	t, err := ioutil.ReadAll(f)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  tmpl, err := template.New("index").Parse(string(t))
-  if err != nil {
-    log.Fatal(err)
-  }
+	tmpl, err := template.New("index").Parse(string(t))
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  log.Println(tmpl)
+	log.Println(tmpl)
 
-  mux := http.NewServeMux()
+	mux := http.NewServeMux()
 
-  mux.HandleFunc("/gene-attributes/", func(w http.ResponseWriter, req *http.Request) {
-    templates := make(map[string]string)
+	mux.HandleFunc("/gene-attributes/", func(w http.ResponseWriter, req *http.Request) {
+		templates := make(map[string]string)
 
-    paths, err := filepath.Glob("gene-attributes/templates/*")
+		paths, err := filepath.Glob("gene-attributes/templates/*")
 
-    for _, path := range paths {
-      f, err   := os.Open(path)
-      if err != nil {
-        log.Fatal(err)
-      }
+		for _, path := range paths {
+			f, err := os.Open(path)
+			if err != nil {
+				log.Fatal(err)
+			}
 
-      t, err := ioutil.ReadAll(f)
-      if err != nil {
-        log.Fatal(err)
-      }
+			t, err := ioutil.ReadAll(f)
+			if err != nil {
+				log.Fatal(err)
+			}
 
-      templates[filepath.Base(path)] = string(t)
-    }
+			templates[filepath.Base(path)] = string(t)
+		}
 
+		buffer, err := json.Marshal(templates)
+		if err != nil {
+			log.Fatal(err)
+		}
 
-    buffer, err := json.Marshal(templates)
-    if err != nil {
-      log.Fatal(err)
-    }
+		f, err := os.Open("gene-attributes/index.js")
+		if err != nil {
+			log.Fatal(err)
+		}
 
-    f, err := os.Open("gene-attributes/index.js")
-    if err != nil {
-      log.Fatal(err)
-    }
+		js, err := ioutil.ReadAll(f)
 
-    js, err := ioutil.ReadAll(f)
+		b := Bindings{
+			TemplatesJson: string(buffer),
+			IndexJs:       string(js),
+		}
+		err = tmpl.Execute(w, b)
+		if err != nil {
+			log.Fatal(err)
+		}
+	})
 
-    b := Bindings{
-      TemplatesJson: string(buffer),
-      IndexJs:       string(js),
-    }
-    err = tmpl.Execute(w, b)
-    if err != nil {
-      log.Fatal(err)
-    }
-  })
+	mux.Handle("/", http.FileServer(http.Dir(".")))
 
-  mux.Handle("/", http.FileServer(http.Dir(".")))
-
-  http.ListenAndServe(":8080", mux)
+	http.ListenAndServe(":8080", mux)
 }
